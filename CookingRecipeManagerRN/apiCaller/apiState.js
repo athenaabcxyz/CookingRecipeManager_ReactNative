b095ab47c58cfe5bebd687f4e6870b8f303841f2
Tenrousa Athena <athenaabcxyz@gmail.com>
2023-05-03 14:42:24 +0700

    Re init project

---
+++ CookingRecipeManagerRN/apiCaller/apiState.js
@@ -0,0 +1,3 @@
+const [assets, setAssets] = useState([]);
+    const [apilink, setapilink] = useState("https://testnets-api.opensea.io/api/v1/assets");
+    const [isFetching, setFetching] = useState(false);
