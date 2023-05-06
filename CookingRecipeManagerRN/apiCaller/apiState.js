c28222d6410566a0988ba16e122eb2ed6c3b499e
Tenrousa Athena <athenaabcxyz@gmail.com>
2023-05-06 13:52:21 +0700

    Delete project

--- CookingRecipeManagerRN/apiCaller/apiState.js
+++
@@ -1,3 +0,0 @@
-const [assets, setAssets] = useState([]);
-    const [apilink, setapilink] = useState("https://testnets-api.opensea.io/api/v1/assets");
-    const [isFetching, setFetching] = useState(false);
